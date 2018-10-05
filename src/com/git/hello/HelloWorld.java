017fcc7c7f4208e0da200d32f46642c220738c91
Administrator <Administrator@windows10.microdone.cn>
2018-10-05 13:09:03 +0800

    test

--- src/com/git/hello/HelloWorld.java
+++ src/com/git/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package com.git.hello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! 修改一下...");
 	}
 }
